b63d4ec971dd31c4f73ff0db5142dc3892a128ad
Sébastien Mosser <sebastien.mosser@gmail.com>
2012-03-01 21:33:27 +0100

    SensApp system library

---
+++ net.modelbased.sensapp.library.system/src/main/scala/net/modelbased/sensapp/library/system/Service.scala
@@ -0,0 +1,35 @@
+/**
+ * This file is part of SensApp [ http://sensapp.modelbased.net ]
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Sebastien Mosser <sebastien.mosser@sintef.no>
+ *
+ * Module: net.modelbased.sensapp.library.system
+ *
+ * SensApp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SensApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with SensApp. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package net.modelbased.sensapp.library.system
+
+import cc.spray._
+import cc.spray.http._
+import cc.spray.json._
+import cc.spray.json.DefaultJsonProtocol._
+import cc.spray.directives._
+import cc.spray.typeconversion.SprayJsonSupport
+
+trait Service extends Directives with SprayJsonSupport {
+  val service: RequestContext => Unit
+ 
+}
---
+++ net.modelbased.sensapp.library.system/src/main/scala/net/modelbased/sensapp/library/system/System.scala
@@ -0,0 +1,49 @@
+/**
+ * This file is part of SensApp [ http://sensapp.modelbased.net ]
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Sebastien Mosser <sebastien.mosser@sintef.no>
+ *
+ * Module: net.modelbased.sensapp.library.system
+ *
+ * SensApp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SensApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with SensApp. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package net.modelbased.sensapp.library.system
+
+import akka.config.Supervision._
+import akka.actor.Supervisor
+import akka.actor.Actor._
+import akka.actor.ActorRef
+import cc.spray._
+
+trait System {
+
+  def services: List[Service]
+  
+  
+  def bootstrap() = {
+    println("pouet")
+    println(services)
+    var actorRefs : List[ActorRef] = services map { s => actorOf(new HttpService(s.service))}
+    val root = actorOf(new RootService(actorRefs.head, actorRefs.tail: _*))
+    val supervisors = actorRefs map {Supervise(_,Permanent)}
+    Supervisor(
+      SupervisorConfig(
+        OneForOneStrategy(List(classOf[Exception]), 3, 100),
+        Supervise(root, Permanent) :: supervisors))
+  }
+  
+  bootstrap
+}
