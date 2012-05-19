762b1994216ea46fcbb8f1ef8208978590eaff3e
Sébastien Mosser <sebastien.mosser@gmail.com>
2012-05-19 14:32:02 +0200

    Gatling with license headers

--- net.modelbased.sensapp.backyard.gatling/src/main/scala/Recorder.scala
+++ net.modelbased.sensapp.backyard.gatling/src/main/scala/Recorder.scala
@@ -1,3 +1,25 @@
+/**
+ * This file is part of SensApp [ http://sensapp.modelbased.net ]
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Sebastien Mosser <sebastien.mosser@sintef.no>
+ *
+ * Module: net.modelbased.sensapp.backyard.gatling
+ *
+ * SensApp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SensApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with SensApp. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 import com.excilys.ebi.gatling.core.util.PathHelper.path2string
 import com.excilys.ebi.gatling.recorder.configuration.CommandLineOptionsConstants.{ REQUEST_BODIES_FOLDER_OPTION, PACKAGE_OPTION, OUTPUT_FOLDER_OPTION }
 import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
