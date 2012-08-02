91e2821a8088839855afcd0046dc3cc2e6874533
brice-morin <brice.morin1@gmail.com>
2012-08-02 11:29:26 +0200

    PoC to parse CSV

--- net.modelbased.sensapp.service.converter/src/main/scala/net/modelbased/sensapp/service/sample/Service.scala
+++ net.modelbased.sensapp.service.converter/src/main/scala/net/modelbased/sensapp/service/sample/Service.scala
@@ -4,7 +4,7 @@
  * Copyright (C) 2012-  SINTEF ICT
  * Contact: Sebastien Mosser <sebastien.mosser@sintef.no>
  *
- * Module: net.modelbased.sensapp.archetype.service
+ * Module: net.modelbased.sensapp.service.converter
  *
  * SensApp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -22,6 +22,12 @@
  */
 package net.modelbased.sensapp.service.sample
 
+import au.com.bytecode.opencsv.{CSVReader}
+
+import java.io.FileReader
+
+import java.text.{DateFormat, SimpleDateFormat}
+
 import cc.spray._
 import cc.spray.http._
 import cc.spray.json._
@@ -29,15 +35,42 @@ import cc.spray.json.DefaultJsonProtocol._
 import cc.spray.directives._
 import cc.spray.typeconversion.SprayJsonSupport
 
+import scala.collection.JavaConversions._
 
+
+import java.util.Locale
 import net.modelbased.sensapp.library.system.{Service => SensAppService} 
 
 trait Service extends SensAppService {
   
-  override val name = "service.sample"
+  override implicit val partnerName = "service.sample"
   
   val service = {
     path("sample" / "elements") {
       get { _ complete "foo" }
     }}
+}
+
+
+object TestParser {
+    
+  def main(args : Array[String]) {
+      val reader : CSVReader = new CSVReader(new FileReader("C:/work/OBD/trackLog-2012-Aug-01_08-46-13.csv"), ',', '\\', 1);
+      val myEntries = reader.readAll();
+      
+      val dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.US)
+
+      myEntries.foreach{line =>
+        println("DATE:" + line(0))
+        try {
+          println("DATE: " + dateFormat.parse(line(0)))
+        } catch {
+          case e : java.text.ParseException => println("Cannot parse timestamp, ignoring line")
+        }
+        println(line.mkString("[", ", ", "]"))
+      }
+  }
+   
+  
+  
 }
