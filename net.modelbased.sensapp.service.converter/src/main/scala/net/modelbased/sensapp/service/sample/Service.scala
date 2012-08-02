38280433190ed20ee76d25fdc268d97ad9330c11
Sébastien Mosser <sebastien.mosser@gmail.com>
2012-08-02 10:28:03 +0200

    converter skel

---
+++ net.modelbased.sensapp.service.converter/src/main/scala/net/modelbased/sensapp/service/sample/Service.scala
@@ -0,0 +1,43 @@
+/**
+ * This file is part of SensApp [ http://sensapp.modelbased.net ]
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Sebastien Mosser <sebastien.mosser@sintef.no>
+ *
+ * Module: net.modelbased.sensapp.archetype.service
+ *
+ * SensApp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SensApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with SensApp. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package net.modelbased.sensapp.service.sample
+
+import cc.spray._
+import cc.spray.http._
+import cc.spray.json._
+import cc.spray.json.DefaultJsonProtocol._
+import cc.spray.directives._
+import cc.spray.typeconversion.SprayJsonSupport
+
+
+import net.modelbased.sensapp.library.system.{Service => SensAppService} 
+
+trait Service extends SensAppService {
+  
+  override val name = "service.sample"
+  
+  val service = {
+    path("sample" / "elements") {
+      get { _ complete "foo" }
+    }}
+}
