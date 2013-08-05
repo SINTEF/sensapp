7f46cfd96abb4bb0614365e39c1ddb5fc59f0c42
Jonathan Nain <jonathan.nain@gmail.com>
2013-08-05 16:44:23 +0200

    Gatling Module for HTTP OK.

--- net.modelbased.sensapp.backyard.gatling/src/main/scala/Recorder.scala
+++ net.modelbased.sensapp.backyard.gatling/src/main/scala/Recorder.scala
@@ -1,32 +1,12 @@
-/**
- * This file is part of SensApp [ http://sensapp.modelbased.net ]
- *
- * Copyright (C) 2011-  SINTEF ICT
- * Contact: SINTEF ICT <nicolas.ferry@sintef.no>
- *
- * Module: net.modelbased.sensapp
- *
- * SensApp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * SensApp is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General
- * Public License along with SensApp. If not, see
- * <http://www.gnu.org/licenses/>.
- */
-import com.excilys.ebi.gatling.core.util.PathHelper.path2string
-import com.excilys.ebi.gatling.recorder.configuration.CommandLineOptionsConstants.{ REQUEST_BODIES_FOLDER_OPTION, PACKAGE_OPTION, OUTPUT_FOLDER_OPTION }
-import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
-
-import IDEPathHelper.{ requestBodiesFolder, recorderOutputFolder }
+import io.gatling.recorder.config.RecorderPropertiesBuilder
+import io.gatling.recorder.controller.RecorderController
 
 object Recorder extends App {
 
-	GatlingHttpProxyUI.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder, PACKAGE_OPTION, "net.modelbased.sensapp.backyard.gatling", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
+  val props = new RecorderPropertiesBuilder
+  props.simulationOutputFolder(IDEPathHelper.recorderOutputDirectory.toString)
+  props.simulationPackage("GatlingTest")
+  props.requestBodiesFolder(IDEPathHelper.requestBodiesDirectory.toString)
+
+  RecorderController(props.build, Some(IDEPathHelper.recorderConfigFile))
 }
--- net.modelbased.sensapp.backyard.gatling/src/main/scala/net/modelbased/sensapp/backyard/gatling/Helpers.scala
+++ net.modelbased.sensapp.backyard.gatling/src/main/scala/net/modelbased/sensapp/backyard/gatling/Helpers.scala
@@ -1,37 +1,18 @@
-/**
- * This file is part of SensApp [ http://sensapp.modelbased.net ]
- *
- * Copyright (C) 2011-  SINTEF ICT
- * Contact: SINTEF ICT <nicolas.ferry@sintef.no>
- *
- * Module: net.modelbased.sensapp
- *
- * SensApp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * SensApp is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General
- * Public License along with SensApp. If not, see
- * <http://www.gnu.org/licenses/>.
- */
 package net.modelbased.sensapp.backyard.gatling
 
-import com.excilys.ebi.gatling.core.Predef._
-import com.excilys.ebi.gatling.http.Predef._
-import com.excilys.ebi.gatling.jdbc.Predef._
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jonathan
+ * Date: 05/08/13
+ * Time: 12:44
+ */
 
 object RandomData {
-  private[this] val bag = new scala.util.Random	
+  private[this] val bag = new scala.util.Random
   def apply(sensorId: String, stamp: Long): String = {
     val data = "{\"e\":[{\"n\":\""+ sensorId + "\", \"u\": \"m\", \"v\": " + bag.nextFloat() +",\"t\": "+ stamp +"}]}"
     data
-  } 
+  }
 }
 
 object RandomSensor {
