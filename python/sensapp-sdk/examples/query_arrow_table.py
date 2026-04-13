f778b3c2b75d9fee5f5f458501db81e9dbf37a3b
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2026-04-13 14:05:59 +0000

    feat: 🔥 remove previous sdk

--- python/sensapp-sdk/examples/query_arrow_table.py
+++
@@ -1,6 +0,0 @@
-from sensapp_sdk import SensAppClient
-
-with SensAppClient("http://127.0.0.1:3000") as client:
-    table = client.query_arrow('temperature{room="lab"}[6h]')
-    print(table.schema)
-    print(table.to_pylist()[:5])
